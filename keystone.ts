f4522d2dbc2df6f9a053f69bd6091552aaca7926
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2021-03-14 12:39:31 +0530

    chore: adds alternative frontend URL

--- keystone.ts
+++ keystone.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import {createAuth} from '@keystone-next/auth'
+import { createAuth } from '@keystone-next/auth';
 import {
   withItemData,
-  statelessSessions,
+  statelessSessions
 } from '@keystone-next/keystone/session';
 
 import { User } from './schemas/User';
@@ -17,7 +17,7 @@ const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/ecommerce';
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long a user is signed in
-  secret: process.env.COOKIE_SECRET,
+  secret: process.env.COOKIE_SECRET
 };
 
 const { withAuth } = createAuth({
@@ -27,31 +27,30 @@ const { withAuth } = createAuth({
   initFirstItem: {
     fields: ['name', 'email', 'password']
   }
-})
-
+});
 
 export default withAuth(
   config({
     // @ts-ignore
     server: {
       cors: {
-        origin: [process.env.FRONTEND_URL],
-        credentials: true,
-      },
+        origin: [process.env.ALT_FRONTEND_URL],
+        credentials: true
+      }
     },
     db: {
       adapter: 'mongoose',
       url: databaseURL,
       onConnect: async (keystone) => {
         if (process.argv.includes('--seed-data')) {
-          await insertSeedData(keystone)          
+          await insertSeedData(keystone);
         }
       }
     },
     lists: createSchema({
       User,
       Product,
-      ProductImage,
+      ProductImage
       // Category,
       // SubCategory,
     }),
@@ -59,7 +58,7 @@ export default withAuth(
       // Show the UI only for poeple who pass this test
       isAccessAllowed: ({ session }) =>
         // console.log(session);
-        Boolean(session?.data),
+        Boolean(session?.data)
     },
     session: withItemData(statelessSessions(sessionConfig), {
       User: 'id name email'
